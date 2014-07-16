0f324ea22106add6e376d0eba8b7100a502f22ae
Jorge Ferrer <jorge.ferrer@liferay.com>
2014-07-16 13:15:32 +0200

    LMW-12 Rename button and small format changes

--- ios/Library/Source/Login/LoginView.swift
+++ ios/Library/Source/Login/LoginView.swift
@@ -15,7 +15,7 @@ import UIKit
 
 enum AuthType: String {
 	case Email = "Email"
-	case ScreenName = "Screen name"
+	case ScreenName = "Screen Name"
 }
 
 class LoginView: BaseWidgetView, UITextFieldDelegate {
