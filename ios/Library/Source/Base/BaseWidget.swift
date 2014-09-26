1c9a6bd5d4ce422207f85a1acab332e3aefeee4c
JM <jose.navarro@liferay.com>
2014-09-26 13:04:07 +0200

    LMW-25 Live render is done in the center of the widget.
    Preview image uses the smallest size possible
    
    Conflicts:
    	ios/Library/Source/Base/BaseWidget.swift

--- ios/Library/Source/Base/BaseWidget.swift
+++ ios/Library/Source/Base/BaseWidget.swift
@@ -118,13 +118,7 @@ import QuartzCore
 
 		if runningOnInterfaceBuilder {
 			if let currentPreviewImageValue = currentPreviewImage {
-				let imageRect = CGRectMake(
-						(frame.size.width - currentPreviewImageValue.size.width)/2,
-						0,
-						currentPreviewImageValue.size.width,
-						currentPreviewImageValue.size.height)
-
-				previewLayer.frame = imageRect
+				previewLayer.frame = centeredRectInWidget(size: currentPreviewImageValue.size)
 				previewLayer.contents = currentPreviewImageValue.CGImage
 
 				if previewLayer.superlayer != layer {
@@ -158,10 +152,11 @@ import QuartzCore
 		let view = createWidgetViewFromNib();
 
 		if let viewValue = view {
-			viewValue.frame = bounds
+			viewValue.frame = centeredRectInWidget(size: viewValue.frame.size)
 			viewValue.onUserAction = onUserAction;
 
 			addSubview(viewValue)
+			sendSubviewToBack(viewValue)
 
 			return viewValue
 		}
@@ -176,8 +171,6 @@ import QuartzCore
 			result = UIImage(contentsOfFile: previewImagePath)
 		}
 		else if let widgetView = createWidgetViewFromNib() {
-			widgetView.frame = bounds
-
 			result = previewImageFromView(widgetView)
 		}
 
@@ -223,7 +216,10 @@ import QuartzCore
 	}
 
 	internal func previewImageFromView(view:UIView) -> UIImage {
-		UIGraphicsBeginImageContextWithOptions(view.frame.size, false, 0.0)
+		let previewWidth = min(view.frame.size.width, self.frame.size.width)
+		let previewHeight = min(view.frame.size.height, self.frame.size.height)
+
+		UIGraphicsBeginImageContextWithOptions(CGSizeMake(previewWidth, previewHeight), false, 0.0)
 
 		view.layer.renderInContext(UIGraphicsGetCurrentContext())
 		let previewImage = UIGraphicsGetImageFromCurrentImageContext()
@@ -331,6 +327,14 @@ import QuartzCore
 		let themeName = currentThemeName
 
 		currentPreviewImage = previewImageForTheme(themeName)
+	}
+
+	private func centeredRectInWidget(#size: CGSize) -> CGRect {
+		return CGRectMake(
+				(self.frame.size.width - size.width) / 2,
+				(self.frame.size.height - size.height) / 2,
+				size.width,
+				size.height)
 	}
 
 }
